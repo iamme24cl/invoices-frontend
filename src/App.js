8090b688bf9d8743512b52cc516811bb4a85eaa2
Chandra Lama <iamme24cl@gmail.com>
2021-05-02 15:11:48 -0700

    testing fetching data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/accounts/1/invoices')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
   render () {
     return (
       <div className="App">
