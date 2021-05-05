ad37a73fd012e621cccb43f5d6fd4a0462e3a21f
Chandra Lama <iamme24cl@gmail.com>
2021-05-05 15:37:22 -0700

    set up index file and implemented store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { thunk } from 'redux-thunk';
 import { Provider } from 'react-redux'
 
@@ -9,9 +9,13 @@ import './index.css';
 import App from './App';
 
 // set up our store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
