befdd1785cecbd9b8eadd8369a07af95428c057d
Chandra Lama <iamme24cl@gmail.com>
2021-05-05 13:10:50 -0700

    tested fetching data with backend API

--- src/App.js
+++ src/App.js
@@ -53,14 +53,14 @@ class App extends React.Component {
       payment_due: "2021-08-19",
       description: "New Brand",
       payment_terms: 1,
-      status: "Paid",
+      status: "paid",
       client_name: "Jensen Huang",
       client_email: "jensenh@mail.com",
       client_address: "106 Kendell Street, Sharrington, NR24 5WQ, United Kingdom",
       items_attributes: [
         {
           name: "Brand Guidelines",
-          price: 2000.00,         
+          price: 1000.00,         
           quantity: 2
         }
       ]
@@ -84,7 +84,7 @@ class App extends React.Component {
   }
 
   handleDelete = () => {
-    fetch('http://localhost:3000/api/v1/accounts/1/invoices/2', {
+    fetch('http://localhost:3000/api/v1/accounts/1/invoices/1', {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
