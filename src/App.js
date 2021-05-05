a2972ed9ee7d6169be269a2e9167df9ab0202438
Chandra Lama <iamme24cl@gmail.com>
2021-05-05 11:46:26 -0700

    tested updating and deletting invoices

--- src/App.js
+++ src/App.js
@@ -48,6 +48,53 @@ class App extends React.Component {
 
   }
 
+  handleUpdate = () => {
+    const UpdateData = {
+      payment_due: "2021-08-19",
+      description: "New Brand",
+      payment_terms: 1,
+      status: "Paid",
+      client_name: "Jensen Huang",
+      client_email: "jensenh@mail.com",
+      client_address: "106 Kendell Street, Sharrington, NR24 5WQ, United Kingdom",
+      items_attributes: [
+        {
+          name: "Brand Guidelines",
+          price: 2000.00,         
+          quantity: 2
+        }
+      ]
+    }
+
+    fetch('http://localhost:3000/api/v1/accounts/1/invoices/1', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(UpdateData),      
+    })
+    .then(response => response.json())
+    .then(dataObj => {
+      console.log('Success:', dataObj)
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+    });     
+
+  }
+
+  handleDelete = () => {
+    fetch('http://localhost:3000/api/v1/accounts/1/invoices/2', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }, 
+      body: null
+    })
+    .then(response => response.json())
+    .then(data => alert(data.message));
+  }
+
 
 
   render () {
@@ -55,6 +102,8 @@ class App extends React.Component {
       <div className="App">
        App
        <button onClick={this.handleOnClick}>Post Data</button>
+       <button onClick={this.handleDelete}>Delete Data</button>
+       <button onClick={this.handleUpdate}>Update Data</button>
       </div>
     );
   }
