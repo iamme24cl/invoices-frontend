8bfcc29c93cb9ec1ac9a924333e5b6cd8cc79ce8
Chandra Lama <iamme24cl@gmail.com>
2021-05-25 09:01:04 -0700

    minor refactor

--- src/components/account/Accounts.js
+++ src/components/account/Accounts.js
@@ -1,6 +1,5 @@
 import React from 'react' 
-import {Route, Link} from 'react-router-dom'
-import Account from './Account'
+import {Link} from 'react-router-dom'
 
 const Accounts = (props) => {
   return (
