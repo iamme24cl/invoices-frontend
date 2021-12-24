b833f07eae5777d7fef53dd185eb8959bf3cb1d1
chandra <iamme24cl@gmail.com>
2021-12-24 11:53:04 -0800

    created navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,21 +11,21 @@ const NavBar = (props) => {
 
   const toggleNavbar = () => {
     setIsOpen(!isOpen);
-  }
+  };
 
   const handleLogout = (id) => {
     logout();
     history.push("/");
-  }
+  };
 
   return (
-    <nav className="bg-gray-900 sm:flex sm:justify-between sm:px-5 text-white">
+    <nav className="bg-gray-300 shadow-xl sm:flex sm:justify-between sm:px-5 text-black no-print">
       <div className="flex items:center justify-between px-5 py-3">
         <div>
-          <Link className="cursor-pointer" to="/">INVOICES</Link>
+          <Link className="cursor-pointer font-bold" to="/">INVOICES</Link>
         </div>
         <div className="sm:hidden">
-          <button type="button" onClick={toggleNavbar} className="text-gray-500 hover:text-white focus:text-white focus:outline-none block">
+          <button type="button" onClick={toggleNavbar} className="text-black hover:gray-500 focus:text-gray-500 focus:outline-none block">
             <svg className="h-8 w-8 fill-current" viewBox="0 0 24 24">
               {isOpen && <path fillRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>}
               {!isOpen && <path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>}
@@ -34,12 +34,16 @@ const NavBar = (props) => {
         </div>
       </div>
       <div className={`px-3 pt-2 pb-4 sm:flex sm:items-center ${isOpen ? "block": "hidden"}`}>
-        {!account.id && <Link to="/login" className="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Login</Link>}
+        {!account.id && 
+          <>
+            <Link to="/login" className="block px-3 py-1 text-black  font-semibold rounded hover:bg-gray-400">Login</Link>
+            <Link to="/signup" className="block px-3 py-1 text-black  font-semibold rounded hover:bg-gray-400">Signup</Link>
+          </>  
+        }
         {account.id &&
           <> 
-            <Link to="/" className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-3">Home</Link>
-            <Link to="/invoices" className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-3">Invoices</Link>
-            <button onClick={handleLogout} className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-3">Logout</button>
+            <Link to="/" className="block px-3 py-1 text-black  font-semibold rounded hover:bg-gray-400">Home</Link>
+            <button onClick={handleLogout} className="block px-3 py-1 text-black  font-semibold rounded hover:bg-gray-400">Logout</button>
           </>
         }
       </div>
